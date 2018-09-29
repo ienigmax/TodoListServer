286633917b82b6b70dbfda6f7644fb07a0f1e95d
iEnigmaX <alege92@outlook.com>
2018-09-29 19:40:56 +0300

    Migrations to create tables for SQLite

---
+++ database/migrations/2018_09_29_083657_create_task_list.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid')->unique();
+            $table->string('title')->nullable();
+            $table->string('content')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
