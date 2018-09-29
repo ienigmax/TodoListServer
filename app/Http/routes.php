3822bc2c5c83c2e1450f3c841a585122e1b56510
iEnigmaX <alege92@outlook.com>
2018-09-29 19:44:06 +0300

    API - add routes, they lead to the controller. Each route requests the relevant function in the controller class. Added a default routing in case a user mistakes in typing for some reason - leads to '404' page view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::resource('/api/v1/tasks', 'TaskController');
+
+Route::get('/api/v1/tasks/get_all_tasks', 'TaskController@getAllTasks');
+Route::get('/api/v1/tasks/add_new_task', 'TaskController@addNewTask');
+Route::get('/api/v1/tasks/delete_task', 'TaskController@removeExistingTask');
+Route::get('/api/v1/tasks/change_status', 'TaskController@changeTaskStatus');
+Route::any('/api/v1/tasks/{param}', function(){
+   return view('errors/404');
 });
