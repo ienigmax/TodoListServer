6f2fb0ac777b849088dfe6c2563c1acf5c25c4f1
iEnigmaX <alege92@outlook.com>
2018-09-29 19:45:01 +0300

    The task list controller - the main app logic is here, speaks with the model

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\TaskModel;
+
+class TaskController extends Controller
+{
+
+    public function getAllTasks() {
+
+        // Get all tasks
+        $taskList = TaskModel::all();
+
+        if(empty($taskList)){
+            return ['success' => false, 'error' => 'empty list', 'data' => null];
+        } else {
+            return ['success' => true, 'error' => null, 'data' => $taskList];
+        }
+    }
+
+
+    public function addNewTask(Request $request) {
+
+        $params = $request->query();
+
+        if((!isset($params['title'])) or (empty($params['title']))){
+            return ['success' => false, 'error' => 'title missing'];
+        } else if((!isset($params['content'])) or (empty($params['content']))){
+            return ['success' => false, 'error' => 'content missing'];
+        }
+
+        $result = TaskModel::insertTask(['title' => $params['title'], 'content' => $params['content']]);
+
+        if((isset($result)) && ($result !== 0)){
+            return ['success' => true, 'error' => null, 'data' => $result];
+        } else {
+            return ['success' => false, 'error' => 'insert failed', 'data' => null];
+        }
+
+    }
+
+
+    public function removeExistingTask(Request $request) {
+
+        $params = $request->query();
+        if((!isset($params['uuid'])) or (empty($params['uuid']))){
+            return ['success' => false, 'error' => 'uuid missing'];
+        }
+
+        $result = TaskModel::deleteTask($params['uuid']);
+
+        if((isset($result)) && ($result !== 0)){
+            return ['success' => true, 'error' => null, 'data' => $result];
+        } else {
+            return ['success' => false, 'error' => 'remove failed', 'data' => null];
+        }
+
+    }
+
+    public function changeTaskStatus(Request $request) {
+
+        $params = $request->query();
+
+        if((!isset($params['status'])) or (empty($params['status']))){
+            return ['success' => false, 'error' => 'status missing'];
+        } else if((!isset($params['uuid'])) or (empty($params['uuid']))){
+            return ['success' => false, 'error' => 'uuid missing'];
+        }
+
+        $result = TaskModel::updateTaskStatus($params['uuid'], $params['status']);
+
+        if((isset($result)) && ($result !== 0)){
+            return ['success' => true, 'error' => null, 'data' => $result];
+        } else {
+            return ['success' => false, 'error' => 'update failed', 'data' => null];
+        }
+
+    }
+
+}
