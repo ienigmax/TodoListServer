0e3ca273bc05cb2c1ef71ae76fddc10e935f7d21
iEnigmaX <alege92@outlook.com>
2018-10-01 11:31:51 +0300

    Add update title and update content functionality + fix column updated at not being updated to the current timestamp despite the update

--- app/TaskModel.php
+++ app/TaskModel.php
@@ -27,7 +27,21 @@ class TaskModel extends Model
     public static function updateTaskStatus($uuid, $status){
 
         $self = new static;
-        return DB::update('UPDATE ' . $self->table . ' SET status=? WHERE uuid=?', [$status, $uuid]);
+        return DB::update('UPDATE ' . $self->table . ' SET status=?, updated_at=? WHERE uuid=?', [$status, time(), $uuid]);
+
+    }
+
+    public static function updateTaskTitle($uuid, $title){
+
+        $self = new static;
+        return DB::update('UPDATE ' . $self->table . ' SET title=?, updated_at=? WHERE uuid=?', [$title, time(), $uuid]);
+
+    }
+
+    public static function updateTaskContent($uuid, $content){
+
+        $self = new static;
+        return DB::update('UPDATE ' . $self->table . ' SET content=?, updated_at=? WHERE uuid=?', [$content, time(), $uuid]);
 
     }
 }
