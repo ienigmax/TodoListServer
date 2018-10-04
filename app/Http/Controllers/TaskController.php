93ed7c4c330baf38fbea12f4a264e5bdb754c0a5
iEnigmaX <alege92@outlook.com>
2018-10-04 08:25:53 +0300

    Fix the base64 check function

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -137,7 +137,7 @@ class TaskController extends Controller
     }
 
     private function is_base64_encoded($data){
-        if (preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $data)) {
+        if (preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $data)) {
            return TRUE;
         } else {
             return FALSE;
